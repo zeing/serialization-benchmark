571b1f2186bf81e44c551a72fdd54f5d4fce06f2
Zeing <zeing@outlook.com>
2025-03-07 00:39:37 +0700

    update benchmark implementation and README for clarity and accuracy

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ A performance benchmark comparing different serialization libraries in Node.js:
 This benchmark tests the speed and efficiency of **packing (serialization)** and **unpacking (deserialization)** to evaluate which library performs best under various conditions.
 
 ### Install
+
 To install dependencies:
 
 ```bash
@@ -26,9 +27,10 @@ bun run benchmark.ts
 ### Result:
 
 `Running benchmark with 100,000 iterations...`
-| | msgpackr | notepack.io | binarytf |
-| ------- | --------- | ----------- | ----------- |
-| 1 | 102.16 ms | 102.16 ms |263.07 ms|
-| 2 | 87.77 ms | 150.98 ms |259.90 ms|
-| 3 | 103.46 ms | 133.34 ms |313.09 ms|
+| | msgpackr (pack/unpack) | msgpackr (encode/decode) | notepack.io | binarytf |
+| ------- | --------- | ----------- | ----------- | ----------- |
+| 1 | 102.16 ms |56.42 ms| 102.16 ms |263.07 ms|
+| 2 | 87.77 ms |56.13 ms| 150.98 ms |259.90 ms|
+| 3 | 103.46 ms |55.96 ms| 133.34 ms |313.09 ms|
+
 # serialization-benchmark
--- benchmark.ts
+++ benchmark.ts
@@ -1,21 +1,22 @@
-import { pack, unpack } from 'msgpackr';
-import notepack from 'notepack.io';
-import { serialize, deserialize } from 'binarytf';
+import { pack, unpack, encode, decode } from 'msgpackr'
+import notepack from 'notepack.io'
+import { serialize, deserialize } from 'binarytf'
 
-const data = { name: "ZEING", age: 3, features: ["Hi", "I'm", "Fastest"], active: true, score: 99.9 };
-const iterations = 100000;
+const data = { name: 'ZEING', age: 3, features: ['Hi', "I'm", 'Fastest'], active: true, score: 99.9 }
+const iterations = 100_000
 
 function benchmark(name: string, packFunc: any, unpackFunc: any) {
-    const start = performance.now();
-    for (let i = 0; i < iterations; i++) {
-        const packed = packFunc(data);
-        unpackFunc(packed);
-    }
-    const duration = performance.now() - start;
-    console.log(`${name}: ${duration.toFixed(2)} ms`);
+  const start = performance.now()
+  for (let i = 0; i < iterations; i++) {
+    const packed = packFunc(data)
+    unpackFunc(packed)
+  }
+  const duration = performance.now() - start
+  console.log(`${name}: ${duration.toFixed(2)} ms`)
 }
 
-console.log(`Running benchmark with ${iterations} iterations...`);
-benchmark("msgpackr", pack, unpack);
-benchmark("notepack.io", notepack.encode, notepack.decode);
-benchmark("binarytf", serialize, deserialize);
+console.log(`Running benchmark with ${iterations} iterations...`)
+benchmark('msgpackr pack/unpack', pack, unpack)
+benchmark('msgpackr encode/decode', encode, decode)
+benchmark('notepack.io', notepack.encode, notepack.decode)
+benchmark('binarytf', serialize, deserialize)
